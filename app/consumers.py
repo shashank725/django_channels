95eba1ab9f03cac19eac80a7af8d2361d316ec3a
shashank725 <shashank.nov725@gmail.com>
2023-12-23 21:24:56 +0530

    SyncConsumer & AsyncConsumer

--- app/consumers.py
+++ app/consumers.py
@@ -1,11 +1,12 @@
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
-
+from time import sleep
+import asyncio
 
 class MySyncConsumer(SyncConsumer):
     
     def websocket_connect(self, event):
-        # Called on connection.
+        # Called on connection and is about to finish the websocket handshake.
         print("Websocket connected...", event)
         self.send({
             'type': 'websocket.accept'
@@ -15,6 +16,12 @@ class MySyncConsumer(SyncConsumer):
         # Called when data is received from the client
         print("Message received...", event)
         print("Messaged is:", event['text'])
+        for i in range(50):
+            self.send({
+            'type': 'websocket.send',
+            'text': str(i)
+            })
+            sleep(1)
 
     def websocket_disconnect(self, event):
         # Called when the socket closes
@@ -25,7 +32,7 @@ class MySyncConsumer(SyncConsumer):
 class MyAsyncConsumer(AsyncConsumer):
 
     async def websocket_connect(self, event):
-        # Called on connection.
+        # Called on connection and is about to finish the websocket handshake.
         print("Websocket connected...", event)
         await self.send({
             'type': 'websocket.accept'
@@ -35,6 +42,12 @@ class MyAsyncConsumer(AsyncConsumer):
         # Called when data is received from the client
         print("Message received...", event)
         print("Messaged is:", event['text'])
+        for i in range(50):
+            await self.send({
+            'type': 'websocket.send',
+            'text': str(i)
+            })
+            await asyncio.sleep(1)
 
     async def websocket_disconnect(self, event):
         # Called when the socket closes
